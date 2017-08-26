f8cd1b30a10a8b1d3a140bb5f8505650fdc3692c
My Name <test@exmaple.com>
2017-08-26 19:57:54 +0900

    update build

--- build.sample.sh
+++ build.sample.sh
@@ -1,37 +1,4 @@
-import resolve from 'rollup-plugin-node-resolve';
-import replace from 'rollup-plugin-replace';
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
-
-import path from 'path';
-import fs from  'fs';
-
-const selfImport = option => {
-    const root = __dirname;
-    const pkg = JSON.parse(fs.readFileSync(path.join(root, 'package.json')));
-    const main = path.join(root, pkg.main);
-    return {
-        resolveId: ( importee, importer )  => {
-            return (importee === pkg.name) ? main : null;
-        }
-    }
-};
-
-export default {
-    plugins: [
-        replace({ 'process.env.NODE_ENV': process.env.NODE_ENV }),
-        babel({
-            babelrc: false,
-            exclude: 'node_modules/**',
-            presets: ['react'],
-            plugins: ['external-helpers']
-        }),
-        resolve({
-            browser: true,
-            main: true,
-            extensions: [ '.js', '.jsx' ]
-        }),
-        commonjs(),
-        selfImport(),
-    ]
-};
+npx rollup --config rollup.config.sample.js -format=es -i sample/pure/index.jsx -o sample/pure/index.js &
+npx rollup --config rollup.config.sample.js -format=es -i sample/port/index.jsx -o sample/port/index.js &
+npx rollup --config rollup.config.sample.js -format=es -i sample/flow/index.jsx -o sample/flow/index.js &
+wait
